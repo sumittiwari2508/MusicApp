b4564870334fc27be3dbfb75f49e9d5070f24b78
sumittiwari2508 <sumit.k@bizfab.com>
2021-01-25 00:12:02 -0500

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Kasun on 11/6/1399 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
