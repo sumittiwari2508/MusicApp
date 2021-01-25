fedfd739b6f67d7de4d774705ef5833cbfe5d00f
sumittiwari2508 <sumit.k@bizfab.com>
2021-01-25 03:09:37 -0500

    Project uploaded

---
+++ MusicApp/CollectionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionTableViewCell.swift
+//  MusicApp
+//
+//  Created by Kasun on 11/6/1399 AP.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MusicApp/collectionCell/musicCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  musicCollectionViewCell.swift
+//  MusicApp
+//
+//  Created by Kasun on 11/6/1399 AP.
+//
+
+import UIKit
+
+class musicCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
